7d782755bc11432a70dd9095c9a339ebb6f8d180
Habert kungu <habert231@gmail.com>
2024-11-25 22:13:26 +0300

    Added solution for removing Element

--- Arrays/duplicates.py
+++ Arrays/duplicates.py
@@ -1,10 +1,7 @@
 class Solution(object):
     def removeDuplicates(self, nums):
         numbers_set = set()
-        # setcounter to maintain the new size of the Array
         counter = 0
-        # loop thru the arr and compare the two numbers added
-        # in the set and the one in the arr
         for i in range(len(nums)):
             if nums[i] not in numbers_set:
                 numbers_set.add(nums[i])
